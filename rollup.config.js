60a59d40d19955a6c320d7c7bd8668b3818fd297
Anil Anar <anilanar@hotmail.com>
2022-02-27 20:57:04 +0100

    Enable es module exports, rm side effects on load

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,39 @@
-import uglify from 'rollup-plugin-uglify';
+import { defineConfig } from 'rollup';
+import { uglify } from 'rollup-plugin-uglify';
 
-export default [
+export default defineConfig([
   {
     input: 'src/focus-visible.js',
     output: {
-      file: 'dist/focus-visible.js',
+      name: 'focusVisible',
+      file: 'dist/umd/focus-visible.js',
       format: 'umd'
     }
   },
   {
     input: 'src/focus-visible.js',
     output: {
-      file: 'dist/focus-visible.min.js',
+      name: 'focusVisible',
+      file: 'dist/umd/focus-visible.min.js',
       format: 'umd',
       sourcemap: true
     },
     plugins: [uglify()]
+  },
+  {
+    input: 'src/focus-visible.js',
+    output: {
+      file: 'dist/cjs/focus-visible.js',
+      format: 'cjs',
+      sourcemap: true
+    }
+  },
+  {
+    input: 'src/focus-visible.js',
+    output: {
+      file: 'dist/es/focus-visible.js',
+      format: 'es',
+      sourcemap: true
+    }
   }
-];
+]);
