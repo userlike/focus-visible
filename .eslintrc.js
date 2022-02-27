a60c6623c337d8562970f8d8d753b4913b87e3ae
Anil Anar <anilanar@hotmail.com>
2022-02-27 22:09:49 +0100

    Convert to TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,10 @@
  */
 module.exports = {
   env: {
-    browser: true,
+    browser: true
   },
-  // The only thing we want ESLint to do is yell at us if we accidentally use ES2015+.
-  // This is because we don't want/need to use Babel in this project.
-  // We specifically DO NOT want ESLint to try to enforce a style guide on us.
-  // We use prettier for that.
-  plugins: ['es5'],
-  extends: ['plugin:es5/no-es2015', 'plugin:es5/no-es2016', 'prettier'],
-  parserOptions: {
-    ecmaVersion: 2015,
-    sourceType: 'module',
-  },
-  rules: {
-    'es5/no-modules': 'off',
-  },
+  plugins: ['@typescript-eslint'],
+  extends: ['prettier'],
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended']
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,43 @@
 import { defineConfig } from 'rollup';
+import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
 export default defineConfig([
   {
-    input: 'src/focus-visible.js',
+    input: 'src/focus-visible.ts',
     output: {
       name: 'focusVisible',
       file: 'dist/umd/focus-visible.js',
-      format: 'umd'
-    }
+      format: 'umd',
+    },
+    plugins: [typescript()],
   },
   {
-    input: 'src/focus-visible.js',
+    input: 'src/focus-visible.ts',
     output: {
       name: 'focusVisible',
       file: 'dist/umd/focus-visible.min.js',
       format: 'umd',
-      sourcemap: true
+      sourcemap: true,
     },
-    plugins: [terser()]
+    plugins: [typescript(), terser()],
   },
   {
-    input: 'src/focus-visible.js',
+    input: 'src/focus-visible.ts',
     output: {
       file: 'dist/cjs/focus-visible.js',
       format: 'cjs',
-      sourcemap: true
-    }
+      sourcemap: true,
+    },
+    plugins: [typescript()],
   },
   {
-    input: 'src/focus-visible.js',
+    input: 'src/focus-visible.ts',
     output: {
       file: 'dist/es/focus-visible.js',
       format: 'es',
-      sourcemap: true
-    }
-  }
+      sourcemap: true,
+    },
+    plugins: [typescript()],
+  },
 ]);
