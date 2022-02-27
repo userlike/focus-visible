ea701e3f6b8c8e4ddbf8c092c505e338cfa05670
Anil Anar <anilanar@hotmail.com>
2022-02-27 21:17:45 +0100

    Update lint/prettier to get rid of remaining audit problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
 module.exports = {
   env: {
-    browser: true
+    browser: true,
   },
   // The only thing we want ESLint to do is yell at us if we accidentally use ES2015+.
   // This is because we don't want/need to use Babel in this project.
@@ -9,9 +12,10 @@ module.exports = {
   plugins: ['es5'],
   extends: ['plugin:es5/no-es2015', 'plugin:es5/no-es2016', 'prettier'],
   parserOptions: {
-    sourceType: 'module'
+    ecmaVersion: 2015,
+    sourceType: 'module',
   },
   rules: {
-    'es5/no-modules': 'off'
-  }
+    'es5/no-modules': 'off',
+  },
 };
